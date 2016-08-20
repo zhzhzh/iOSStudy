18fcf8d06bea06bad179364a1624708c021bc531
Zhang, Jie <jzhang14@paypal.com>
2016-08-21 00:27:29 +0800

    create Quiz project

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Zhang, Jie on 8/21/16.
+//  Copyright © 2016 Jie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showQuestion(sender: UIButton) {
+    }
+    
+    @IBAction func showAnswer(sender: UIButton) {
+    }
+    
+
+}
+
