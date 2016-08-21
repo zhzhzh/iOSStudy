4f54419167ee134885311c5dd7619a919aa094c6
Zhang, Jie <jzhang14@paypal.com>
2016-08-21 13:43:00 +0800

    add question and anwner, icon

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -13,9 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     
+    var questionIndex: Int = 0
+    var questionArray:[String] = []
+    var answerArray:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        questionArray = [
+            "From what is congac made?",
+            "What is 7+7?",
+            "What is capital of Vermont"
+        ]
+        
+        answerArray = [
+            "Grapes",
+            "14",
+            "Montpelier"
+        ]
+        
+        questionLabel.text = questionArray[questionIndex]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +43,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showQuestion(sender: UIButton) {
+        questionIndex += 1;
+        if (questionIndex == questionArray.count) {
+            questionIndex = 0
+        }
+        
+        questionLabel.text = questionArray[questionIndex]
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: UIButton) {
+        answerLabel.text = answerArray[questionIndex]
     }
     
 
